836b1793285f7bd6ad112d97fa86b4173135095c
Javier <carriloruiz@gmail.com>
2025-05-02 16:37:10 +0200

    SQL

--- REPASO_SQL/PUBS.sql
+++ REPASO_SQL/PUBS.sql
@@ -6,8 +6,13 @@ CREATE TABLE empleado(
 dni_empleado VARCHAR(20) PRIMARY KEY,
 nombre VARCHAR(30) NOT NULL,
 domicilio VARCHAR(50)
-
 );
+
+INSERT INTO empleado(dni_empleado,nombre,domicilio)
+VALUES 
+        ('22342A','javi','calle pena'),
+        ('53454B','pepe','calle ana'),
+        ('766757E','jose','calle rocio');
 
 CREATE TABLE localidad(
 cod_localidad INT AUTO_INCREMENT,
@@ -15,8 +20,15 @@ nombre VARCHAR(30) NOT NULL,
 PRIMARY KEY(cod_localidad)
 );
 
+INSERT INTO localidad(nombre)
+VALUES 
+        ('cordoba'),
+        ('sevilla'),
+        ('madrid');
+
+
 CREATE TABLE pub(
-cod_pub INT AUTO_INCREMENT,
+cod_pub VARCHAR(20),
 nombre VARCHAR(50) NOT NULL,
 licencia_fiscal VARCHAR(50) NOT NULL,
 domicilio VARCHAR(50),
@@ -27,31 +39,67 @@ PRIMARY KEY (cod_pub),
 FOREIGN KEY (cod_localidad) REFERENCES localidad (cod_localidad)
 );
 
+INSERT INTO pub(cod_pub,nombre,licencia_fiscal,domicilio,fecha_apertura,horario,cod_localidad)
+VALUES
+        ('12345','casilla','licenciado','casa alberto','2025-12-12','hor1',1),
+        ('213456','porteria','lincen','piso pedro','2026-01-12','hor2',2),
+        ('312345','gol','certificado','casa ana','2026-12-12','hor3',3);
+
 CREATE TABLE pub_empleado(
-cod_pub INT AUTO_INCREMENT,
-dni_empleado VARCHAR(20) NOT NULL,
-funcion VARCHAR(50) CHECK (funcion IN('camarero','limpieza','seguridad')) NOT NULL,
+cod_pub VARCHAR(20),
+dni_empleado VARCHAR(20),
+funcion VARCHAR(50) CHECK (funcion IN('camarero','limpieza','seguridad')),
 PRIMARY KEY(cod_pub,dni_empleado),
 FOREIGN KEY (dni_empleado) REFERENCES empleado (dni_empleado)
 );
 
+INSERT INTO pub_empleado(cod_pub,dni_empleado,funcion)
+VALUES 
+        (1234,'22342A','camarero'),
+        (3456,'53454B','limpieza'),
+        (2345,'766757E','seguridad');
+
 CREATE TABLE existencias(
-cod_articulo INT AUTO_INCREMENT,
+cod_articulo VARCHAR(20),
 nombre VARCHAR (50) NOT NULL,
 cantidad INT NOT NULL,
-precio INT CHECK (precio = 0)NOT NULL,
-cod_pub INT NOT NULL,
+precio INT CHECK (precio > 0),
+cod_pub VARCHAR(20) NOT NULL,
 PRIMARY KEY(cod_articulo),
 FOREIGN KEY (cod_pub) REFERENCES pub (cod_pub)
 );
+
+INSERT INTO existencias(cod_articulo,nombre,cantidad,precio,cod_pub)
+VALUES 
+        ('shein','camiseta',2,20,'12345'),
+        ('hm','pantalon',5,50,'213456'),
+        ('zara','top',1,30,'12345');
+
 
 CREATE TABLE titular(
 dni_titular VARCHAR(20),
 nombre VARCHAR(50) NOT NULL,
 domicilio VARCHAR(50),
-cod_pub INT NOT NULL,
+cod_pub VARCHAR(20) NOT NULL,
 PRIMARY KEY(dni_titular),
 FOREIGN KEY (cod_pub) REFERENCES pub (cod_pub)
 );
 
+INSERT INTO titular(dni_titular,nombre,domicilio,cod_pub)
+VALUES 
+        ('534534H','javi','avenida santa ana','12345'),
+        ('342465Y','pedro','calle ciguela','12345'),
+        ('234894J','carmen','casa alma','312345');
 
+
+SELECT * FROM empleado;
+
+SELECT * FROM localidad;
+
+SELECT * FROM pub;
+
+SELECT * FROM pub_empleado;
+
+SELECT * FROM existencias;
+
+SELECT * FROM titular;
---
+++ REPASO_SQL/custonSantos.sql
@@ -0,0 +1,123 @@
+DROP DATABASE IF EXISTS custonSantos;
+CREATE DATABASE custonSantos;
+USE custonSantos;
+
+CREATE TABLE coches(
+mat VARCHAR(8) PRIMARY KEY,
+marca VARCHAR(15),
+an_fab INT CHECK ( an_fab BETWEEN 1000 AND 9999)
+);
+
+
+CREATE TABLE mecanicos(
+dni VARCHAR(19) PRIMARY KEY,
+nombre VARCHAR(15),
+puesto VARCHAR(15),
+parcial CHAR(1)
+);
+
+
+CREATE TABLE trabajos(
+mat VARCHAR(8),
+dni VARCHAR(19),
+horas DECIMAL CHECK ( horas > 0.5),
+fecha_rep DATE,
+FOREIGN KEY (mat) REFERENCES coches (mat),
+FOREIGN KEY (dni) REFERENCES mecanicos (dni)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+CREATE TABLE clientes(
+id_cliente VARCHAR(10) PRIMARY KEY,
+nombre VARCHAR(50),
+telefono VARCHAR (15)
+);
+
+CREATE TABLE piezas(
+id_pieza VARCHAR(10) PRIMARY KEY,
+nombre VARCHAR(50),
+precio DECIMAL (10,2)
+);
+
+CREATE TABLE factura(
+id_factura VARCHAR(10) PRIMARY KEY,
+id_cliente VARCHAR(10),
+fecha_emision DATE,
+total DECIMAL
+);
+
+ALTER TABLE coches ADD COLUMN modelo VARCHAR(15);
+
+ALTER TABLE trabajos ADD CONSTRAINT PK_trabajos PRIMARY KEY (mat,dni);
+
+ALTER TABLE trabajos ADD CONSTRAINT FK_trabajos_mecanicos FOREIGN KEY (dni) REFERENCES mecanicos (dni) 
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+-- Se puede añadir un check asi 
+-- ALTER TABLE coches MODIFY COLUMN an_fab INT, ADD CONSTRAINT chk_an_fab CHECK (an_fab BETWEEN 10 AND 99);
+
+-- O tambien asi se pude anadir el check
+ALTER TABLE coches MODIFY an_fab INT CHECK ( an_fab BETWEEN 10 AND 99);
+
+ALTER TABLE coches DROP CHECK coches_chk_1;
+
+ALTER TABLE trabajos DROP FOREIGN KEY trabajos_ibfk_2;
+
+
+SHOW CREATE TABLE coches;
+
+-- Ver como se llaman los nombres de todos los check
+SELECT CONSTRAINT_NAME
+FROM information_schema.check_constraints
+WHERE CONSTRAINT_SCHEMA = 'custonSantos';
+
+ALTER TABLE coches CHANGE COLUMN marca marca_coche VARCHAR(15);
+
+INSERT INTO coches ( mat,marca_coche,an_fab,modelo)
+VALUES
+       ('3424ff','nissan',25,'pepe'),
+       ('4235r','BUGATTI',24,'quepasabala'),
+       ('5646g','ford', 23,'wtf');
+
+INSERT INTO mecanicos (dni, nombre, puesto, parcial)
+VALUES
+        ('12345678A', 'Carlos', 'Jefe', 'N'),
+        ('87654321B', 'Lucía', 'Ayudante', 'S'),
+        ('11223344C', 'Raúl', 'Técnico', 'N');
+
+INSERT INTO trabajos (mat, dni, horas, fecha_rep)
+VALUES
+  ('3424ff', '12345678A', 2.5, '2024-06-01'),
+  ('4235r', '87654321B', 1.0, '2024-06-10'),
+  ('5646g', '11223344C', 3.0, '2024-07-15');
+
+
+UPDATE coches
+SET marca_coche = UPPER(marca_coche)
+WHERE LENGTH (marca_coche) > 4;
+
+UPDATE coches 
+SET marca_coche = 'lambo'
+WHERE marca_coche = 'NISSAN';
+
+
+UPDATE coches
+SET modelo = 'SUPER'
+WHERE modelo = 'pepe';
+
+SELECT * FROM coches;
+
+SELECT * FROM mecanicos;
+
+
+UPDATE mecanicos
+SET dni = '3333333ZZZ'
+WHERE dni = '11223344C';
+
+SELECT * FROM mecanicos;
+SELECT * FROM trabajos;
+
+
+
